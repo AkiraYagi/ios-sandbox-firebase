854fd4ded528a6f1f1288ab55923babdaf3a0eb9
AkiraYagi <akira.developer7@gmail.com>
2018-10-27 16:32:52 +0900

    Initial Commit

---
+++ FirebaseSandbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseSandbox
+//
+//  Created by AkiraYagi on 2018/10/27.
+//  Copyright © 2018年 AkiraYagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
